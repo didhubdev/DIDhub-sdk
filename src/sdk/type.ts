99992886233f3105b2c1c2225c97b19caf3b163b
Henry Yeung <henrywfyeung@gmail.com>
2023-05-12 15:55:53 +0800

    Updates

--- src/sdk/type.ts
+++ src/sdk/type.ts
@@ -3,5 +3,5 @@ import { IBatchRegister } from '../modules/batchRegister/type';
 export { IBatchRegister }
 
 export interface IDIDhubSDK {
-    did: IBatchRegister;
+    register: IBatchRegister;
 }
