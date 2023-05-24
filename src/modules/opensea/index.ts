38d500e376b5947147d0b5694ae078baa4fafbf7
Henry Yeung <henrywfyeung@gmail.com>
2023-05-24 17:29:53 +0800

    Updates

--- src/modules/opensea/index.ts
+++ src/modules/opensea/index.ts
@@ -56,13 +56,13 @@ export const openseaInit: IOpenseaInit = (
                 },
               ],
               startTime: startTime,
-              endTime: endTime
+              endTime: endTime,
+              conduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000"
             },
             signerAddress
           );
         
         const order = await executeAllActions();
-        order.parameters.conduitKey = "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000";
 
         return order;
     }
