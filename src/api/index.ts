2d8a3988a2047ca6152f75c9f40cc7748fa02c55
Henry Yeung <henrywfyeung@gmail.com>
2025-02-21 13:36:28 +0800

    Updates

--- src/api/index.ts
+++ src/api/index.ts
@@ -36,7 +36,7 @@ export const getOpenseaBasisPoints = async (
   }
 
   const data = await response.json();
-  return data.basisPoints;
+  return data.data.basisPoints;
 }
 
 export const getOpenseaListingData = async (
